1f0d9dbbe72a9b983ee7841933fac227a3af3db1
Shestakov Alexey <Shestakov.a.mail.work@gmail.com>
2020-12-23 08:53:12 +0200

    Changed  Angular Cli version

---
+++ src/app/components/month-switcher/month-switcher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-month-switcher',
+  templateUrl: './month-switcher.component.html',
+  styleUrls: ['./month-switcher.component.scss']
+})
+export class MonthSwitcherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
