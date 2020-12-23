af935d8130c638096426b0c3c44adb69f05363ca
Shestakov Alexey <Shestakov.a.mail.work@gmail.com>
2020-12-23 09:02:43 +0200

    added day,user,team,vacation models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
+import { MonthSwitcherComponent } from './components/month-switcher/month-switcher.component';
+import { CalendarTableComponent } from './components/calendar-table/calendar-table.component';
+import { TableBodyComponent } from './components/table-body/table-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    MonthSwitcherComponent,
+    CalendarTableComponent,
+    TableBodyComponent
   ],
   imports: [
     BrowserModule,
