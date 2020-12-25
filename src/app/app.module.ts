fc3dafaf817235f3fc46e5d7fd9fa311a09384a9
Shestakov Alexey <Shestakov.a.mail.work@gmail.com>
2020-12-25 16:24:41 +0200

    added popup logic, markup and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ModalModule } from "ngx-bootstrap/modal";
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
 import { MonthSwitcherComponent } from './components/month-switcher/month-switcher.component';
@@ -12,6 +13,8 @@ import { DateService } from "./services/date.service";
 import { UserService } from "./services/user.service";
 import { VacationService } from "./services/vacation.service";
 import { PopupComponent } from './components/popup/popup.component';
+
+
 
 @NgModule({
   declarations: [
@@ -25,7 +28,8 @@ import { PopupComponent } from './components/popup/popup.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [
     DateService,
